ccd1f814ad57e16b55474e36fe6b3653a02af3e9
Vinicius Silveira Moraes <vinicius_jag@hotmail.com>
2021-02-10 17:41:20 -0300

    services created, authenticator, hashManager and idGenerator

---
+++ cookenu/src/services/authenticator.ts
@@ -0,0 +1,31 @@
+import * as jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+import { authenticationData } from "../types/types"
+
+dotenv.config()
+
+export const generateToken = (
+    payload: authenticationData
+):string =>{
+
+    const token: string = jwt.sign(
+        payload,
+        process.env.JWT_KEY as string,
+        {expiresIn: process.env.JWT_EXPIRE_TIME as string || "5h"}
+    )
+    return token
+}
+
+export const getTokenData = (
+    token: string
+):authenticationData =>{
+    
+    const payload = jwt.verify(
+        token,
+        process.env.JWT_KEY as string
+    ) as any
+
+    const result = {id: payload.id}
+
+    return result
+}
